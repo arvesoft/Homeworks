95529e2248d067aec66af8ee12b94d0fa134c63f
nbyavuz <byavuz81@gmail.com>
2018-04-17 15:17:52 +0300

    Valgrind 0a cekildi, helgrind duruyor ama zararsiz gibi

--- OS/THE2/yedekmain.c
+++ OS/THE2/yedekmain.c
@@ -7,13 +7,13 @@ sem_t mutex;
 
 struct anttype {
 
-  int x,y,id,timer;
+  int x,y,id;
 
 };
 
 
 void *func(void *ant){
-  int start = time(NULL);
+
   struct anttype *my_ant = (struct anttype*)ant;
   char mystate = '1';
   int x = my_ant->x;
@@ -23,10 +23,6 @@ void *func(void *ant){
   //printf("Thread ID: %d x = %d y = %d\n", my_ant->id, my_ant->x, my_ant->y);
 
   while(1){
-    int now = time(NULL);
-    if(now - start > my_ant->timer)
-      break;
-
     int randomtime = rand() % 10;
 
     usleep(1000 * (getDelay() + randomtime));
@@ -283,6 +279,7 @@ void *func(void *ant){
           putCharTo(y, x, 'P');
         }
 
+
         //          sem_wait(&mutex);
         if(cnt1)
           sem_post(&semaphore[ (oldy) * GRIDSIZE + oldx - 1]);
@@ -310,173 +307,6 @@ void *func(void *ant){
 
         //          sem_post(&mutex);
 
-        if(mystate == '1'){
-          usleep(1000 * (getDelay() + randomtime));
-          oldx = x;
-          oldy = y;
-
-          sem_wait(&mutex);
-
-          if(x - 1 >= 0){
-            sem_wait(&semaphore[ (y) * GRIDSIZE + x - 1]);
-            cnt1 = 1;
-          }
-
-          if(x + 1 < GRIDSIZE){
-            sem_wait(&semaphore[ (y) * GRIDSIZE + x + 1]);
-            cnt2 = 1;
-          }
-
-          if(y - 1 >= 0){
-            sem_wait(&semaphore[ (y - 1) * GRIDSIZE + x]);
-            cnt3 = 1;
-          }
-
-          if(y + 1 < GRIDSIZE){
-            sem_wait(&semaphore[ (y + 1) * GRIDSIZE + x]);
-            cnt4 = 1;
-          }
-
-          if(x - 1 >= 0 && y - 1 >= 0){
-            sem_wait(&semaphore[ (y - 1) * GRIDSIZE + x - 1]);
-            cnt5 = 1;
-          }
-
-          if(x + 1 < GRIDSIZE & y - 1 >= 0){
-            sem_wait(&semaphore[ (y - 1) * GRIDSIZE + x + 1]);
-            cnt6 = 1;
-          }
-
-          if(x - 1 >= 0 && y + 1 < GRIDSIZE){
-            sem_wait(&semaphore[ (y + 1) * GRIDSIZE + x - 1]);
-            cnt7 = 1;
-          }
-
-          if(x + 1 < GRIDSIZE && y + 1 < GRIDSIZE){
-              sem_wait(&semaphore[ (y + 1) * GRIDSIZE + x + 1]);
-              cnt8 = 1;
-          }
-
-          sem_post(&mutex);
-
-          random = rand() % 8;
-          checker = 1;
-
-
-          for(int i = 0 ; i < 8 && checker; i++, random++){
-
-            // Etrafimda yemek yok, sola bakiyorum bos mu diye
-            if(cnt1 && (random % 8 == 0)){
-              if(lookCharAt(y, x-1) == '-'){
-                putCharTo(y, x-1, '1');
-                putCharTo(y , x, '-');
-                x = x - 1;
-                checker = 0;
-              }
-            }
-
-            // Etrafimda yemek yok, saga bakiyorum bos mu diye
-            else if(cnt2 && (random % 8 == 1)){
-              if(lookCharAt(y, x+1) == '-'){
-                putCharTo(y, x+1, '1');
-                putCharTo(y , x, '-');
-                x = x + 1;
-                checker = 0;
-              }
-            }
-
-
-            // Etrafimda yemek yok, uste bakiyorum bos mu diye
-            else if(cnt3 && (random % 8 == 2)){
-              if(lookCharAt(y-1, x) == '-'){
-                putCharTo(y-1, x, '1');
-                putCharTo(y , x, '-');
-                y = y - 1;
-                checker = 0;
-              }
-            }
-
-
-            // Etrafimda yemek yok, alta bakiyorum bos mu diye
-            else if(cnt4 && (random % 8 == 3)){
-              if(lookCharAt(y+1, x) == '-'){
-                putCharTo(y+1, x, '1');
-                putCharTo(y , x, '-');
-                y = y + 1;
-                checker = 0;
-              }
-            }
-
-            // Etrafimda yemek yok, sol uste bakiyorum bos mu diye
-            else if(cnt5 && (random % 8 == 4)){
-              if(lookCharAt(y-1, x-1) == '-'){
-                putCharTo(y - 1, x - 1, '1');
-                putCharTo(y , x, '-');
-                x = x - 1;
-                y = y - 1;
-                checker = 0;
-              }
-            }
-
-            // Etrafimda yemek yok, sag uste bakiyorum bos mu diye
-            else if(cnt6 && (random % 8 == 5)){
-              if(lookCharAt(y-1, x+1) == '-'){
-                putCharTo(y-1, x+1, '1');
-                putCharTo(y , x, '-');
-                x = x + 1;
-                y = y - 1;
-                checker = 0;
-              }
-            }
-
-            // Etrafimda yemek yok, sol alta bakiyorum bos mu diye
-            else if(cnt7 && (random % 8 == 6)){
-              if(lookCharAt(y+1, x-1) == '-'){
-                putCharTo(y+1, x-1, '1');
-                putCharTo(y , x, '-');
-                x = x - 1;
-                y = y + 1;
-                checker = 0;
-              }
-            }
-
-            // Etrafimda yemek yok, sag alta bakiyorum bos mu diye
-            else if(cnt8 && (random % 8 == 7)){
-              if(lookCharAt(y + 1, x + 1) == '-'){
-                putCharTo(y + 1, x + 1, '1');
-                putCharTo(y , x, '-');
-                x = x + 1;
-                y = y + 1;
-                checker = 0;
-              }
-            }
-          }
-
-          if(cnt1)
-            sem_post(&semaphore[ (oldy) * GRIDSIZE + oldx - 1]);
-
-          if(cnt2)
-            sem_post(&semaphore[ (oldy) * GRIDSIZE + oldx + 1]);
-
-          if(cnt3)
-            sem_post(&semaphore[ (oldy - 1) * GRIDSIZE + oldx]);
-
-          if(cnt4)
-            sem_post(&semaphore[ (oldy + 1) * GRIDSIZE + oldx]);
-
-          if(cnt5)
-            sem_post(&semaphore[ (oldy - 1) * GRIDSIZE + oldx - 1]);
-
-          if(cnt6)
-            sem_post(&semaphore[ (oldy - 1) * GRIDSIZE + oldx + 1]);
-
-          if(cnt7)
-            sem_post(&semaphore[ (oldy + 1) * GRIDSIZE + oldx - 1]);
-
-          if(cnt8)
-            sem_post(&semaphore[ (oldy + 1) * GRIDSIZE + oldx + 1]);
-
-        }
 
       }
 
@@ -733,9 +563,6 @@ void *func(void *ant){
 //          sem_post(&mutex);
         }
       }
-      //free(ant);
-      //free(my_ant);
-    return 0;
     }
 
 
@@ -743,6 +570,7 @@ int a,b;
 int main(int argc, char *argv[]) {
     srand(time(NULL));
     int start = time(NULL);
+
     int semmax = GRIDSIZE * GRIDSIZE;
 
     for(int i = 0; i < semmax; i++)
@@ -764,7 +592,7 @@ int main(int argc, char *argv[]) {
     // You must use putCharTo() and lookCharAt() to access/change the grid.
     // You should be delegating ants to separate threads
     int i,j;
-    pthread_t ant[numberofants];
+    pthread_t ant;
     for (i = 0; i < GRIDSIZE; i++) {
         for (j = 0; j < GRIDSIZE; j++) {
             putCharTo(i, j, '-');
@@ -790,8 +618,7 @@ int main(int argc, char *argv[]) {
         antstruct->x = b;
         antstruct->y = a;
         antstruct->id = i;
-        antstruct->timer = maxsim;
-        pthread_create(ant+i, NULL, func, (void*)antstruct);
+        pthread_create(&ant , NULL, func, (void*)antstruct);
     }
 
 
@@ -804,7 +631,6 @@ int main(int argc, char *argv[]) {
     // the function calls, they do not have any access control, you
     // have to ensure that.
     char c;
-
     while (TRUE) {
 
         int now = time(NULL);
@@ -837,9 +663,6 @@ int main(int argc, char *argv[]) {
     // do not forget freeing the resources you get
     endCurses();
 
-    for(int i = 0; i < numberofants; i++)
-      pthread_join(ant[i], NULL);
-    free(antstruct);
-
     return 0;
 }
+
